12749c4f8417ac7bd567b85bd85fbdfd621c19d1
Muzammil <muzammil.devmontdigital@gmail.com>
2025-02-19 08:46:44 +0000

    Create initial migration & model

---
+++ app/Models/Tradeperson.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Tradeperson extends Model
+{
+    //
+    use HasFactory;
+
+    protected $fillable = ['user_id', 'business_name', 'description', 'phone', 'address'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class, 'tradeperson_category');
+    }
+}
---
+++ database/migrations/2025_02_19_082822_tradeperson_categories.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        //
+        Schema::create('tradeperson_categories', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('tradeperson_id')->constrained('tradepersons')->onDelete('cascade');
+            $table->foreignId('category_id')->constrained('categories')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+        Schema::dropIfExists('tradeperson_categories');
+    }
+};
