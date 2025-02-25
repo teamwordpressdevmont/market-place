89167ea749339e402e5323986590f5171bb78535
Umer Hussain <umerhussain.webdeveloper@gmail.com>
2025-02-25 16:45:45 +0500

    updated.

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -24,7 +24,7 @@
             <div>
               <button type="button" class="flex text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" aria-expanded="false" data-dropdown-toggle="dropdown-user">
                 <span class="sr-only">Open user menu</span>
-                <img class="w-8 h-8 rounded-full" src="{{ asset( 'public/storage/aiguy-images/logo.png' ) }}" alt="user photo">
+                <img class="w-8 h-8 rounded-full" src="{{ asset( '/images/user-image.png' ) }}" alt="user photo">
               </button>
             </div>
             <div class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-sm shadow-sm dark:bg-gray-700 dark:divide-gray-600" id="dropdown-user">
