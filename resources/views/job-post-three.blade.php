854d70baa75df510a814da7ff08407f82fa04ed1
Maryam-Rashid24 <maryamrashid.official@gmail.com>
2025-03-04 14:24:57 +0500

    updated

--- resources/views/job-post-three.blade.php
+++ resources/views/job-post-three.blade.php
@@ -263,6 +263,23 @@
                 <button class="text-xs text-[#ababab] font-semibold px-10 py-3 rounded-full">Profile</button>
                 <button class="text-xs text-[#ababab] font-semibold px-10 py-3 rounded-full">Reviews</button>
              </div>
+            <ul class="flex flex-wrap text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:border-gray-700 dark:text-gray-400">
+                <li class="me-2">
+                    <a href="#" aria-current="page" class="inline-block p-4 text-blue-600 bg-gray-100 rounded-t-lg active dark:bg-gray-800 dark:text-blue-500">Profile</a>
+                </li>
+                <li class="me-2">
+                    <a href="#" class="inline-block p-4 rounded-t-lg hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300">Dashboard</a>
+                </li>
+                <li class="me-2">
+                    <a href="#" class="inline-block p-4 rounded-t-lg hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300">Settings</a>
+                </li>
+                <li class="me-2">
+                    <a href="#" class="inline-block p-4 rounded-t-lg hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300">Contacts</a>
+                </li>
+                <li>
+                    <a class="inline-block p-4 text-gray-400 rounded-t-lg cursor-not-allowed dark:text-gray-500">Disabled</a>
+                </li>
+            </ul>
              <!-- Proposal Content -->
              <div class="pt-3 rounded-lg text-xs text-[#ABABAB] h-[500px] overflow-y-auto">
                 <p class="mb-4">Dear Alex,</p>
