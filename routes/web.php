ccff198aff44ae8f23a5781be9c1733337d9373c
Maryam-Rashid24 <maryamrashid.official@gmail.com>
2025-03-04 16:54:25 +0500

    updated

--- routes/web.php
+++ routes/web.php
@@ -31,6 +31,10 @@ Route::get('/job-post-three', function() {
     return view('job-post-three');
 })->name('job-post-three');
 
+Route::get('/job-post-four', function() {
+    return view('job-post-four');
+})->name('job-post-four');
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
