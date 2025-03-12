b2e74a0da606a5b8984c695455e30fadf09d069b
Maryam-Rashid24 <maryamrashid.official@gmail.com>
2025-03-12 15:44:37 +0500

    update

--- resources/views/job-listing/add-edit.blade.php
+++ resources/views/job-listing/add-edit.blade.php
@@ -35,7 +35,7 @@
                     </div>
                     <div class="site_field_col mt-0! mb-7!">
                         <label for="description" class="block text-sm font-bold text-mat">Description</label>
-                        <div class="mt-4">
+                        <div class="mt-4 bg-white">
                             <textarea name="description" id="content" rows="3" class="textarea_editor block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6">
                               {{ old('description', $OrderDetails->description ?? '') }}
                             </textarea>
