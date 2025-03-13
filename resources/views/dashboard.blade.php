df4169659f7bdb4172902f291a41c243de91c9d8
Maryam-Rashid24 <maryamrashid.official@gmail.com>
2025-03-13 12:30:32 +0500

    updated

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,15 +4,15 @@
    <div class="grid grid-cols-1 mb-10 items-start">
       <div>
          <h1 class="font-semibold lg:text-4xl md:text-2xl text-xl mb-2">Welcome, DM</h1>
-         <p class="font-semibold text-sm text-[#222222]">Here is your listings statistic report from January 05 - Feburary 05.</p>
+         <p class="font-semibold text-sm text-mat">Here is your listings statistic report from January 05 - Feburary 05.</p>
       </div>
    </div>
    <div class="grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 xl:gap-9 gap-5 mt-6">
 
-    <div class="bg-white py-5 xl:px-8 px-5 rounded-xl flex justify-between border border-[#22222233]">
+    <div class="bg-white py-5 xl:px-8 px-4 rounded-xl flex justify-between border border-[#22222233]">
        <div>
-          <h3 class="md:text-3xl text-2xl font-bold text-[##222222] md:mb-12 mb-6">    {{ $totalBudget }}         </h3>
-          <p class="text-[#222222] font-bold text-sm">Total Earnings</p>
+          <h3 class="xl:text-3xl text-xl font-bold text-mat md:mb-12 mb-6">    {{ $totalBudget }}         </h3>
+          <p class="text-mat font-bold text-sm">Total Earnings</p>
        </div>
        <div>
           <svg width="69" height="73" viewBox="0 0 69 73" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -22,10 +22,11 @@
              <path d="M17.2085 46.8757H31.0418M17.2085 29.584H44.8752" stroke="#222222" stroke-width="2" stroke-linecap="round"></path>
           </svg>
        </div>
-    </div><div class="bg-white py-5 xl:px-8 px-5 rounded-xl flex justify-between border border-[#22222233]">
+    </div>
+    <div class="bg-white py-5 xl:px-8 px-4 rounded-xl flex justify-between border border-[#22222233]">
        <div>
-          <h3 class="md:text-3xl text-2xl font-bold text-[#222222] md:mb-12 mb-6">10,000</h3>
-          <p class="text-[#222222] font-bold text-sm">Active users</p>
+          <h3 class="xl:text-3xl text-xl font-bold text-mat md:mb-12 mb-6">10,000</h3>
+          <p class="text-mat font-bold text-sm">Active users</p>
        </div>
        <div>
           <svg width="73" height="73" viewBox="0 0 73 73" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -35,10 +36,10 @@
           </svg>
        </div>
     </div>
-    <div class="bg-white py-5 xl:px-8 px-5 rounded-xl flex justify-between border border-[#22222233]">
+    <div class="bg-white py-5 xl:px-8 px-4 rounded-xl flex justify-between border border-[#22222233]">
        <div>
-          <h3 class="md:text-3xl text-2xl font-bold text-[##222222] md:mb-12 mb-6">{{ $completedJobs }}</h3>
-          <p class="text-[#222222] font-bold text-sm">Completed Jobs</p>
+          <h3 class="xl:text-3xl text-xl font-bold text-mat md:mb-12 mb-6">{{ $completedJobs }}</h3>
+          <p class="text-mat font-bold text-sm">Completed Jobs</p>
        </div>
        <div>
           <svg width="69" height="73" viewBox="0 0 69 73" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -50,14 +51,14 @@
        </div>
     </div>
  </div>
-   <div class="grid grid-cols-1 xl:grid-cols-3 md:gap-9 gap-8 mt-8">
-    <div class="col-span-2 rounded-xl pt-5 bg-white border border-[#22222233] h-fit">
-        <h2 class="text-lg font-bold mb-6 text-[#222222] px-5">Recent Jobs</h2>
+   <div class="grid grid-cols-1 xl:grid-cols-2 2xl:grid-cols-3 xl:gap-9 gap-5 mt-8">
+    <div class="2xl:col-span-2 col-span-1 rounded-xl md:pt-5 pt-4 bg-white border border-[#22222233] h-fit">
+        <h2 class="md:text-lg text-md font-bold md:mb-6 mb-4 text-mat px-5">Recent Jobs</h2>
         <div id="table-container" class="overflow-x-auto ">
            <table class="genericTable w-full text-sm text-left rtl:text-right text-gray-500">
               <thead class="text-xs text-gray-700 bg-[#eee] border-t border-b border-[#22222233]">
                  <tr>
-                    <th scope="col" class="px-6 py-3 text-[#ABABAB] font-[500]">Job ID</th>
+                    <th scope="col" class="px-6 py-3 text-[#ABABAB] font-[500] whitespace-nowrap">Job ID</th>
                     <th scope="col" class="px-6 py-3 text-[#ABABAB] font-[500]">Job Name</th>
                     <th scope="col" class="px-6 py-3 text-[#ABABAB] font-[500]">Type</th>
                     <th scope="col" class="px-6 py-3 text-[#ABABAB] font-[500]">Email ID</th>
@@ -74,14 +75,14 @@
                @else
                @foreach ($orders as $order)
                   <tr class="border-b border-[#e9e9e9]">
-                     <th class="px-6 py-5 whitespace-nowrap text-xs font-bold  {{ $order->orderDetail && $order->orderDetail->urgent ? 'text-[#DB4A2B]' : 'text-[#222222]' }}">#{{ $order->id }}</th>
-                     <td class="px-6 py-5 whitespace-nowrap text-xs font-bold {{ $order->orderDetail && $order->orderDetail->urgent ? 'text-[#DB4A2B]' : 'text-[#222222]' }}">{{ optional($order->orderDetail)->title }}</td>
-                     <td class="px-6 py-5 whitespace-nowrap text-xs font-bold {{ $order->orderDetail && $order->orderDetail->urgent ? 'text-[#DB4A2B]' : 'text-[#222222]' }}">
+                     <th class="px-6 py-5 whitespace-nowrap text-xs font-bold  {{ $order->orderDetail && $order->orderDetail->urgent ? 'text-[#DB4A2B]' : 'text-mat' }}">#{{ $order->id }}</th>
+                     <td class="px-6 py-5 whitespace-nowrap text-xs font-bold {{ $order->orderDetail && $order->orderDetail->urgent ? 'text-[#DB4A2B]' : 'text-mat' }}">{{ optional($order->orderDetail)->title }}</td>
+                     <td class="px-6 py-5 whitespace-nowrap text-xs font-bold {{ $order->orderDetail && $order->orderDetail->urgent ? 'text-[#DB4A2B]' : 'text-mat' }}">
                         @if($order->orderDetail)
                            {{ $order->orderDetail->urgent ? 'Urgent' : 'Flexible' }}
                         @endif
                      </td>
-                     <td class="px-6 py-5 whitespace-nowrap text-xs font-normal {{ $order->orderDetail && $order->orderDetail->urgent ? 'text-[#DB4A2B]' : 'text-[#222222]' }}">{{ $order->tradeperson->user->email }}</td>
+                     <td class="px-6 py-5 whitespace-nowrap text-xs font-normal {{ $order->orderDetail && $order->orderDetail->urgent ? 'text-[#DB4A2B]' : 'text-mat' }}">{{ $order->tradeperson->user->email }}</td>
                     <td class="px-6 py-5 whitespace-nowrap text-xs text-right">
                          <div class="site_user_dropdown">
                             <div class="cursor-pointer w-[30px] ml-auto bg-[#eee] px-1 py-2 rounded-md flex justify-center items-center hover:bg-gray-300 transition" data-dropdown-toggle="userDropdown-action-{{ $order->id }}" data-dropdown-placement="bottom-end">
@@ -94,13 +95,13 @@
                             <div id="userDropdown-action-{{ $order->id }}" class="z-10 bg-white divide-y rounded-xl w-[122px] border border-[#d3d3d3] hidden" data-popper-placement="bottom-end">
                                 <ul class="bg-white text-sm rounded-xl overflow-hidden">
                                     <li class="border-b border-[#d3d3d3]">
-                                        <a href="#" class="text-left block px-3 py-3 text-xs font-light transition hover:bg-[#222222] hover:text-white text-[#222222]">Edit</a>
+                                        <a href="#" class="text-left block px-3 py-3 text-xs font-light transition hover:bg-mat hover:text-white text-mat">Edit</a>
                                     </li>
                                     <li class="border-b border-[#d3d3d3]">
-                                        <a href="#" class="text-left block px-3 py-3 text-xs font-light transition hover:bg-[#222222] hover:text-white text-[#222222]">View</a>
+                                        <a href="#" class="text-left block px-3 py-3 text-xs font-light transition hover:bg-mat hover:text-white text-mat">View</a>
                                     </li>
                                     <li class="">
-                                        <a href="#" class="text-left block px-3 py-3 text-xs font-light transition hover:bg-[#222222] hover:text-white text-[#222222]">Delete</a>
+                                        <a href="#" class="text-left block px-3 py-3 text-xs font-light transition hover:bg-mat hover:text-white text-mat">Delete</a>
                                     </li>
                                 </ul>
                             </div>
@@ -117,9 +118,9 @@
            </table>
         </div>
      </div>
-      <div class="rounded-xl p-5 bg-white border border-[#22222233]">
+      <div class="rounded-xl md:p-5 md:px-5 px-4 py-4 bg-white border border-[#22222233]">
          <div class="flex justify-between items-center mb-4">
-            <h2 class="text-lg font-bold">Pending Approvals</h2>
+            <h2 class="md:text-lg text-md font-bold">Pending Approvals</h2>
             <a href="#" class="group text-[#ABABAB] text-xs flex items-center font-semibold hover:text-[#db4a2b] transition">
                View All
                <svg width="14" height="10" viewBox="0 0 14 10" fill="none" xmlns="http://www.w3.org/2000/svg" class="ml-2 group-hover:stroke-[#db4a2b]">
@@ -130,12 +131,12 @@
          </div>
          <div class="tabsWrapper">
             <div class="tabsScroll w-full overflow-x-auto relative">
-               <ul class="flex flex-nowrap border-b bg-gray-200 rounded-full p-1 min-w-max justify-between" id="default-styled-tab" data-tabs-toggle="#default-styled-tab-content" data-tabs-active-classes="text-purple-600 hover:text-purple-600 dark:text-purple-500 dark:hover:text-purple-500 border-purple-600 dark:border-purple-500" data-tabs-inactive-classes="dark:border-transparent text-gray-500 hover:text-gray-600 dark:text-gray-400 border-gray-100 hover:border-gray-300 dark:border-gray-700 dark:hover:text-gray-300" role="tablist">
+               <ul class="flex flex-nowrap border-b bg-gray-200 rounded-full p-1 min-w-max justify-between" id="default-styled-tab" data-tabs-toggle="#default-styled-tab-content" data-tabs-inactive-classes="text-gray-500 hover:text-gray-600 border-gray-100 hover:border-gray-300" role="tablist">
                   <li role="presentation" class="w-full">
-                     <button class="w-full transition text-xs text-[#ababab] font-semibold px-12 py-3 rounded-full text-purple-600 hover:text-purple-600 dark:text-purple-500 dark:hover:text-purple-500 border-purple-600 dark:border-purple-500" data-tabs-target="#styled-profile" type="button" role="tab" aria-controls="profile" aria-selected="true">Accounts</button>
+                     <button class="w-full transition text-xs text-[#ababab] font-semibold px-5 md:py-3 py-2 rounded-full" data-tabs-target="#styled-profile" type="button" role="tab" aria-controls="profile" aria-selected="true">Accounts</button>
                   </li>
                   <li role="presentation" class="w-full">
-                     <button class="w-full transition text-xs text-[#ababab] font-semibold px-12 py-3 rounded-full dark:border-transparent text-gray-500 hover:text-gray-600 dark:text-gray-400 border-gray-100 hover:border-gray-300 dark:border-gray-700 dark:hover:text-gray-300" id="settings-styled-tab" data-tabs-target="#styled-settings" type="button" role="tab" aria-controls="settings" aria-selected="false">Edited Jobs</button>
+                     <button class="w-full transition text-xs text-[#ababab] font-semibold px-5 md:py-3 py-2 rounded-full text-gray-500 hover:text-gray-600 border-gray-100 hover:border-gray-300" id="settings-styled-tab" data-tabs-target="#styled-settings" type="button" role="tab" aria-controls="settings" aria-selected="false">Edited Jobs</button>
                   </li>
                </ul>
             </div>
@@ -148,16 +149,16 @@
                            $initials = strtoupper(substr($order->title, 0, 1)) . (strpos($order->title, ' ') ? strtoupper(substr($order->title, strpos($order->title, ' ') + 1, 1)) : '');
                      @endphp
 
-                     <div class="flex items-center rounded-2xl border border-[#EDE9D0] bg-[#F4F4F4] md:flex-row flex-col py-4 px-3 mb-3">
+                     <div class="flex md:items-center rounded-2xl border border-[#EDE9D0] bg-[#F4F4F4] md:flex-row flex-col py-4 px-3 mb-3 md:gap-0 gap-3">
                            {{-- <img src="{{ $order->image }}" alt="Job Image" class=" rounded-xl object-cover"> --}}
                            <div class="w-10 h-10 flex items-center justify-center rounded-full bg-green-500 text-white font-bold">
                               {{ $initials }}
                         </div>
-                           <div class="lg:px-4 px-3 flex-1">
-                              <h3 class="text-xs font-bold mb-1 text-[#222222]">{{ $order->title }}<span class="font-light"> create his profile</span></h3>
+                           <div class="lg:px-4 md:px-3 flex-1">
+                              <h3 class="text-xs font-bold mb-1 text-mat">{{ $order->title }}<span class="font-light"> create his profile</span></h3>
                               <p class="text-xs text-[#9A9FA5] font-light">{{ $order->created_at->diffForHumans() }}</p>
                            </div>
-                           <a href="#" class="bg-[#222222] text-white text-xs px-4 py-3 leading-[0] rounded-full hover:bg-[#24C500] transition font-[500]">Review Profile</a>
+                           <a href="#" class="bg-mat text-white text-xs px-4 py-3 leading-[0] rounded-full hover:bg-[#24C500] transition font-[500] w-fit">Review Profile</a>
                      </div>
 
                   @endforeach
@@ -173,16 +174,16 @@
                         $initials = strtoupper(substr($order->title, 0, 1)) . (strpos($order->title, ' ') ? strtoupper(substr($order->title, strpos($order->title, ' ') + 1, 1)) : '');
                   @endphp
 
-                       <div class="flex items-center rounded-2xl border border-[#EDE9D0] bg-[#F4F4F4] md:flex-row flex-col py-4 px-3 mb-3">
+                       <div class="flex md:items-center rounded-2xl border border-[#EDE9D0] bg-[#F4F4F4] md:flex-row flex-col py-4 px-3 mb-3 md:gap-0 gap-3">
                               {{-- <img src="/public/images/profile.png" alt="Job Image" class=" rounded-xl object-cover"> --}}
                               <div class="w-10 h-10 flex items-center justify-center rounded-full bg-green-500 text-white font-bold">
                                  {{ $initials }}
                            </div>
-                              <div class="lg:px-4 px-3 flex-1">
-                              <h3 class="text-xs font-bold mb-1 text-[#222222]">{{ $order->title }} </h3>
+                              <div class="lg:px-4 md:px-3 flex-1">
+                              <h3 class="text-xs font-bold mb-1 text-mat">{{ $order->title }} </h3>
                               <p class="text-xs text-[#9A9FA5] font-light">{{ $order->created_at->diffForHumans() }}</p>
                               </div>
-                              <a href="#" class="bg-[#222222] text-white text-xs px-6 py-3 leading-[0] rounded-full hover:bg-[#24C500] transition font-[500]">Review Job</a>
+                              <a href="#" class="bg-mat text-white text-xs px-4 py-3 leading-[0] rounded-full hover:bg-[#24C500] transition font-[500] w-fit">Review Job</a>
                         </div>
 
                   @endforeach
